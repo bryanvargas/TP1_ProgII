ea09113f4b5af1907b1850279e27f4d0e2ff8d0e
Rodolfo Vargas <bryanprsvar@gmail.com>
2014-04-05 17:38:47 -0300

    Se Implemento Ejercicio SyE

---
+++ TP1_ProgII/src/com/ejercicio/juego/sye/Casillero.java
@@ -0,0 +1,25 @@
+package com.ejercicio.juego.sye;
+
+public class Casillero {
+
+	public int getPosicion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void abandonarCasillero(Jugador jugador) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void ingresarACasillero(Jugador jugador) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Casillero moverYPosicionar(int movimiento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ TP1_ProgII/src/com/ejercicio/juego/sye/Jugador.java
@@ -0,0 +1,47 @@
+package com.ejercicio.juego.sye;
+
+
+
+public class Jugador {
+	private Casillero casillero;
+	private String nomJugador;
+	
+	public Jugador(String nomJugador){
+		this.nomJugador = nomJugador;
+		this.casillero = null;		
+	}
+	
+	public Casillero getCasillero () {
+		 return casillero ;
+	 	}
+		
+ 	public void setCasillero ( Casillero casillero ) {
+		 this.casillero = casillero ;
+ 		}
+ 	
+ 	public String getNomJugador(){
+ 		return nomJugador;
+ 	}
+ 	
+ 	public int posicion(){
+		return casillero.getPosicion();
+ 		
+ 	}
+ 	
+ 	public void avanzar(int movimiento){
+ 		assert movimiento >0 : "no hay moviientos negativos";
+ 		casillero.abandonarCasillero(this);
+ 		casillero = casillero.moverYPosicionar(movimiento);
+ 		casillero.ingresarACasillero(this);
+ 		
+ 	}
+ 	
+ 	@Override
+ 	public String toString() { 		
+ 		return this.nomJugador;
+ 	}
+	
+	
+	
+
+}
